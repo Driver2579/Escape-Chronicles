f74f0ea5e736869e292fb997826f9a6ecbcc18eb
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2025-04-01 01:08:58 +0300

    Create a blank InventorySystem plugin

---
+++ Plugins/InventorySystem/Source/InventorySystem/InventorySystem.Build.cs
@@ -0,0 +1,29 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class InventorySystem : ModuleRules
+{
+	public InventorySystem(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+		
+		PublicDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"Core",
+				"NetCore",
+				"GameplayTags",
+				"GameplayAbilities",
+			}
+			);
+		
+		PrivateDependencyModuleNames.AddRange(
+			new string[]
+			{
+				"CoreUObject",
+				"Engine",
+			}
+			);
+	}
+}
