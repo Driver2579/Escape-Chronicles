7a117a56b700567c7b102c59853506a43e061ae1
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2025-04-11 21:00:01 +0300

    Created fragments system plugin

---
+++ Plugins/FragmentsSystem/Source/FragmentsSystem/Public/Objects/Fragment.h
@@ -0,0 +1,13 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Fragment.generated.h"
+
+// Base class for fragments
+UCLASS(EditInlineNew, Abstract)
+class FRAGMENTATIONSYSTEM_API UFragment : public UObject
+{
+	GENERATED_BODY()
+};
