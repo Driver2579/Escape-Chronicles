7c5fb73a86fbf1b04254915ed1d4dcef2f4be78d
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2025-04-29 15:35:56 +0300

    Init crafting system

--- Plugins/InventorySystem/Source/InventorySystem/Public/ActorComponents/InventoryManagerComponent.h
+++ Plugins/InventorySystem/Source/InventorySystem/Public/ActorComponents/InventoryManagerComponent.h
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
+
 #include "GameplayTagContainer.h"
 #include "InventorySystemGameplayTags.h"
 #include "Common/Structs/FastArraySerializers/InventorySlotsTypedArrayContainer.h"
---
+++ Plugins/InventorySystem/Source/InventorySystem/Public/Common/Structs/InventoryItemCraftData.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "InventoryItemCraftData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FInventoryItemCraftData : public FTableRowBase
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	FName TestName;
+};
