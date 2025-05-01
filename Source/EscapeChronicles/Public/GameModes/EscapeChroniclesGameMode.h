2b63c412d0201f035858d0f4a189c0f03ffeedf0
Driver2579 <driver2579@gmail.com>
2025-05-01 20:20:07 +0300

    Added more documentation to EscapeChroniclesGameMode

--- Source/EscapeChronicles/Public/GameModes/EscapeChroniclesGameMode.h
+++ Source/EscapeChronicles/Public/GameModes/EscapeChroniclesGameMode.h
@@ -25,17 +25,33 @@ protected:
 		const FString& Options, const FString& Portal = L"") override;
 
 private:
+	// A global FUniquePlayerIdManager for generating FUniquePlayerIDs in PlayerState
 	FUniquePlayerIdManager UniquePlayerIdManager;
 
+	// Whether the game has finished the loading regardless of whether it was successful or not
 	bool bInitialGameLoadFinishedOrFailed = false;
 
+	// Called when the game finished loading regardless of whether the loading was successful or not
 	void OnInitialGameLoadFinishedOrFailed();
 
+	/**
+	 * Players in this list are the players that joined the game before the game has finished its loading. They are
+	 * going to be loaded when the loading is finished regardless of whether the loading was successful or not.
+	 */
 	TArray<TWeakObjectPtr<APlayerController>> PlayersWaitingToBeLoaded;
 
+	/**
+	 * Attempts to load the player now if he already has a pawn. If the player doesn't have a pawn yet, then he will be
+	 * loaded once he gets it.
+	 */
 	void LoadPlayerNowOrWhenPawnIsPossessed(APlayerController* PlayerController) const;
 
+	/**
+	 * Called after the player has possessed a pawn for the first time in case we tried to load the player but failed
+	 * because he didn't have a pawn yet.
+	 */
 	void OnPlayerToLoadPawnChanged(APawn* NewPawn) const;
 
+	// Loads the player from the last save game object that was saved or loaded if any
 	void LoadPlayerOrGenerateUniquePlayerIdForPlayer(const APlayerController* PlayerController) const;
 };
