5feb6133bf95b68e563e763e839f3219f043d8ea
Driver2579 <driver2579@gmail.com>
2025-05-09 20:11:42 +0300

    Implemented PlayerOwnershipComponent

---
+++ Source/EscapeChronicles/Private/Components/ActorComponents/PlayerOwnershipComponent.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/ActorComponents/PlayerOwnershipComponent.h"
+
+#include "Net/UnrealNetwork.h"
+
+void UPlayerOwnershipComponent::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(ThisClass, OwningPlayer);
+}
