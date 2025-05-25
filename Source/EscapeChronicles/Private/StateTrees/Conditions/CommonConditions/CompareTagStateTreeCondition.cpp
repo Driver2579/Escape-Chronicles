a24ddb8bb192d5e3e928f78e80854a95a4a1cd7b
Driver2579 <driver2579@gmail.com>
2025-05-25 15:00:11 +0300

    Moved CompareTagStateTreeCondition to CommonConditions folder

--- Source/EscapeChronicles/Private/StateTrees/Conditions/CommonConditions/CompareTagStateTreeCondition.cpp
+++ Source/EscapeChronicles/Private/StateTrees/Conditions/CommonConditions/CompareTagStateTreeCondition.cpp
@@ -1,6 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "StateTree/Conditions/CompareTagStateTreeCondition.h"
+#include "StateTree/Conditions/CommonConditions/CompareTagStateTreeCondition.h"
 
 #include "StateTreeExecutionContext.h"
 
