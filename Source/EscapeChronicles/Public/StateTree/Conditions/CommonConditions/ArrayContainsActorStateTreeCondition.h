cb76ff31bf5728a61ac694da33485e6684925b9c
Driver2579 <driver2579@gmail.com>
2025-06-01 17:26:50 +0300

    Removed unused include from all state tree conditions

--- Source/EscapeChronicles/Public/StateTree/Conditions/CommonConditions/ArrayContainsActorStateTreeCondition.h
+++ Source/EscapeChronicles/Public/StateTree/Conditions/CommonConditions/ArrayContainsActorStateTreeCondition.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "StateTreeConditionBase.h"
-#include "Tasks/StateTreeAITask.h"
 #include "ArrayContainsActorStateTreeCondition.generated.h"
 
 USTRUCT()
--- Source/EscapeChronicles/Public/StateTree/Conditions/CommonConditions/ArrayContainsSuspiciousCharactersStateTreeCondition.h
+++ Source/EscapeChronicles/Public/StateTree/Conditions/CommonConditions/ArrayContainsSuspiciousCharactersStateTreeCondition.h
@@ -4,7 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "StateTreeConditionBase.h"
-#include "Tasks/StateTreeAITask.h"
 #include "ArrayContainsSuspiciousCharactersStateTreeCondition.generated.h"
 
 USTRUCT()
