7522a33420b95715efdbc51f719425a74827e447
Driver2579 <driver2579@gmail.com>
2025-06-06 01:10:37 +0300

    Implemented guards using punches when they are fighting the character. Implemented guards fighting the prisoner when they see him punching someone. Implemented guards fighting the prisoner when he punches them.

---
+++ Source/EscapeChronicles/Private/StateTrees/Evaluators/AIEvaluators/CombatStateTreeEvaluator.cpp
@@ -0,0 +1,82 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StateTree/Evaluators/AIEvaluators/CombatStateTreeEvaluator.h"
+
+#include "AbilitySystemComponent.h"
+#include "StateTreeExecutionContext.h"
+#include "Common/Structs/CombatEvents.h"
+
+void FCombatStateTreeEvaluator::TreeStart(FStateTreeExecutionContext& Context) const
+{
+	FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	ClearEvaluatorData(InstanceData);
+
+	// Subscribe to the punch hit event to know when someone performs a punch to some target
+	InstanceData.OnPunchHitDelegateHandle = FCombatEvents::OnPunchHit.AddRaw(this,
+		&FCombatStateTreeEvaluator::OnPunchHit);
+}
+
+void FCombatStateTreeEvaluator::OnPunchHit(UAbilitySystemComponent* Instigator, UAbilitySystemComponent* Target,
+	FActiveGameplayEffectHandle TargetAppliedEffectHandle) const
+{
+#if DO_ENSURE
+	ensureAlways(IsValid(Instigator));
+	ensureAlways(IsValid(Target));
+#endif
+
+	// Construct the event payload to send it to the StateTree in the next tick
+	LastPunchHitPayload = FOnPunchHitStateTreeEventPayload(Instigator, Target, TargetAppliedEffectHandle);
+
+	/**
+	 * Clear the list of instances that sent the last payload to send the new event in the next tick, but keep the
+	 * allocated space for them because it will be reused in the next tick.
+	 */
+	InstancesThatSentLastPunchHitPayload.Reset();
+}
+
+void FCombatStateTreeEvaluator::Tick(FStateTreeExecutionContext& Context, const float DeltaTime) const
+{
+	// Wait for the next tick if the payload is not valid yet
+	if (!LastPunchHitPayload.IsValid())
+	{
+		return;
+	}
+
+	const FInstanceDataType& InstanceData = Context.GetInstanceData(*this);
+
+	// Return if the last punch event for this node instance was already sent in the previous tick
+	if (InstancesThatSentLastPunchHitPayload.Contains(&InstanceData))
+	{
+		return;
+	}
+
+#if DO_ENSURE
+	ensureAlways(InstanceData.OnPunchHitEventTag.IsValid());
+#endif
+
+	// Send an event with a valid payload to the StateTree
+	Context.SendEvent(InstanceData.OnPunchHitEventTag, FConstStructView::Make(LastPunchHitPayload));
+
+	/**
+	 * Remember that this node instance has sent an event to avoid sending it again in the next tick until the
+	 * OnPunchHit function is called again.
+	 */
+	InstancesThatSentLastPunchHitPayload.Add(&InstanceData);
+}
+
+void FCombatStateTreeEvaluator::TreeStop(FStateTreeExecutionContext& Context) const
+{
+	ClearEvaluatorData(Context.GetInstanceData(*this));
+}
+
+void FCombatStateTreeEvaluator::ClearEvaluatorData(FInstanceDataType& InstanceData) const
+{
+	// Reset the parameters
+	LastPunchHitPayload.Reset();
+	InstancesThatSentLastPunchHitPayload.Empty();
+
+	// Unsubscribe from the event once the evaluator is stopped to avoid crashes
+	FCombatEvents::OnPunchHit.Remove(InstanceData.OnPunchHitDelegateHandle);
+	InstanceData.OnPunchHitDelegateHandle.Reset();
+}
---
+++ Source/EscapeChronicles/Public/StateTree/Tasks/CommonTasks/GetCharacterFromAbilitySystemComponentStateTreeTask.h
@@ -0,0 +1,42 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StateTreeTaskBase.h"
+#include "GetCharacterFromAbilitySystemComponentStateTreeTask.generated.h"
+
+class AEscapeChroniclesCharacter;
+class UAbilitySystemComponent;
+
+USTRUCT()
+struct FGetCharacterFromAbilitySystemComponentStateTreeTaskInstanceData
+{
+	GENERATED_BODY()
+
+	// Ability System Component to get the character from
+	UPROPERTY(EditAnywhere, Category="Input")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	UPROPERTY(EditAnywhere, Category="Output")
+	TObjectPtr<AEscapeChroniclesCharacter> OutCharacter;
+};
+
+/**
+ * Gets the character from the given Ability System Component. The character must be an AvatarActor of the Ability
+ * System Component or the task will fail.
+ */
+USTRUCT(meta=(DisplayName="Get Character From Ability System Component"))
+struct FGetCharacterFromAbilitySystemComponentStateTreeTask : public FStateTreeTaskCommonBase
+{
+	GENERATED_BODY()
+
+	FGetCharacterFromAbilitySystemComponentStateTreeTask();
+
+	using FInstanceDataType = FGetCharacterFromAbilitySystemComponentStateTreeTaskInstanceData;
+
+	virtual const UStruct* GetInstanceDataType() const override { return FInstanceDataType::StaticStruct(); }
+
+	virtual EStateTreeRunStatus EnterState(FStateTreeExecutionContext& Context,
+		const FStateTreeTransitionResult& Transition) const override;
+};
