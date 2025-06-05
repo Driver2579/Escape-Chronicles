5f1f907a6ea475d933ab5b899a8b0e20b59adfec
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2025-06-05 10:16:22 +0300

    Created FCombatEvents

---
+++ Source/EscapeChronicles/Public/Common/Structs/CombatEvents.h
@@ -0,0 +1,20 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+class UAbilitySystemComponent;
+struct FActiveGameplayEffectHandle;
+
+struct FCombatEvents
+{
+	/**
+	 * Broadcasted when a punch successfully hits a target.
+	 * @param Instigator The ability system component of the actor who performed the punch.
+	 * @param Target The ability system component of the actor who was hit by the punch.
+	 * @param TargetAppliedEffectHandle Handle to the gameplay effect applied to the target as a result of the punch.
+	 */
+	DECLARE_MULTICAST_DELEGATE_ThreeParams(FOnPunchHit, UAbilitySystemComponent* Instigator,
+		UAbilitySystemComponent* Target, FActiveGameplayEffectHandle TargetAppliedEffectHandle);
+
+	static FOnPunchHit OnPunchHit;
+};
