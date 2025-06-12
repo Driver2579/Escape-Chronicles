de78d3c2c9d52e8ad69d266218924183294e4f0c
Driver2579 <driver2579@gmail.com>
2025-06-12 13:23:19 +0300

    Implemented the component that changes the visibility of another scene component based on the current game time

---
+++ Source/EscapeChronicles/Private/Components/ActorComponents/SceneComponentVisibilitySchedulerComponent.cpp
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/ActorComponents/SceneComponentVisibilitySchedulerComponent.h"
+
+#include "GameState/EscapeChroniclesGameState.h"
+
+USceneComponentVisibilitySchedulerComponent::USceneComponentVisibilitySchedulerComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void USceneComponentVisibilitySchedulerComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AEscapeChroniclesGameState* GameState = GetWorld()->GetGameState<AEscapeChroniclesGameState>();
+
+	if (!ensureAlways(IsValid(GameState)))
+	{
+		return;
+	}
+
+	// Cache the pointer to the target component based on the reference set in the editor
+	TargetComponent = CastChecked<USceneComponent>(TargetComponentReference.GetComponent(GetOwner()),
+		ECastCheckedType::NullAllowed);
+
+#if DO_ENSURE
+	ensureAlwaysMsgf(TargetComponent.IsValid(),
+		TEXT("No target component is set for visibility scheduling, this component will work for nothing! "
+			"Consider removing this component or setting the target component."));
+#endif
+
+	// Initialize the visibility of the component based on the current game time
+	UpdateComponentVisibility(GameState->GetCurrentGameDateTime().Time, true);
+
+	// Listen for game date time changes
+	GameState->OnCurrentGameDateTimeUpdated.AddUObject(this, &ThisClass::OnCurrentGameDateTimeUpdated);
+}
+
+void USceneComponentVisibilitySchedulerComponent::OnCurrentGameDateTimeUpdated(const FGameplayDateTime& OldDateTime,
+	const FGameplayDateTime& NewDateTime)
+{
+	// Update the visibility of the component based on the new game time
+	UpdateComponentVisibility(NewDateTime.Time);
+}
+
+void USceneComponentVisibilitySchedulerComponent::UpdateComponentVisibility(
+	const FGameplayTime& CurrentGameTime, const bool bForceUpdate)
+{
+	// Whether the component should be visible. Depends on the current game time.
+	bool bNewVisibility;
+
+	// Usual scenario. Example: 10:00-11:30.
+	if (ComponentVisibleStartTime < ComponentVisibleEndTime)
+	{
+		bNewVisibility = CurrentGameTime >= ComponentVisibleStartTime && CurrentGameTime < ComponentVisibleEndTime;
+	}
+	// Special scenario. Example: 22:00-01:30.
+	else
+	{
+		bNewVisibility = CurrentGameTime >= ComponentVisibleStartTime || CurrentGameTime < ComponentVisibleEndTime;
+	}
+
+	// No need to update visibility if it hasn't changed unless the force update was requested
+	if (!bForceUpdate && bNewVisibility == bCurrentVisibility)
+	{
+		return;
+	}
+
+	// Update the visibility of the target component and propagate the visibility change to children if needed
+	if (ensureAlways(TargetComponent.IsValid()))
+	{
+		TargetComponent->SetVisibility(bNewVisibility, bPropagateVisibilityToChildren);
+	}
+
+	// Remember the new visibility state
+	bCurrentVisibility = bNewVisibility;
+}
---
+++ Source/EscapeChronicles/Public/Components/ActorComponents/SceneComponentVisibilitySchedulerComponent.h
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Common/Structs/GameplayDateTime.h"
+#include "SceneComponentVisibilitySchedulerComponent.generated.h"
+
+// Changes the visibility of the selected scene component based on the current game time
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class ESCAPECHRONICLES_API USceneComponentVisibilitySchedulerComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	USceneComponentVisibilitySchedulerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	virtual void OnCurrentGameDateTimeUpdated(const FGameplayDateTime& OldDateTime,
+		const FGameplayDateTime& NewDateTime);
+
+private:
+	// The time when the component should start being visible
+	UPROPERTY(EditAnywhere)
+	FGameplayTime ComponentVisibleStartTime;
+
+	// The time when the component should end being visible
+	UPROPERTY(EditAnywhere)
+	FGameplayTime ComponentVisibleEndTime;
+
+	// Component to change the visibility based on the current game time on
+	UPROPERTY(EditAnywhere, DisplayName="Target Component", meta=(UseComponentPicker, AllowedClasses="SceneComponent"))
+	FComponentReference TargetComponentReference;
+
+	// An actual pointer to the target component. It is set in BeginPlay() based on TargetComponentReference.
+	TWeakObjectPtr<USceneComponent> TargetComponent;
+
+	// If true, the visibility change will be propagated to all children of the target component
+	UPROPERTY(EditAnywhere)
+	bool bPropagateVisibilityToChildren = false;
+
+	/**
+	 * Updates the visibility of the target component based on the current game time.
+	 * @param CurrentGameTime Current game time to check the visibility against.
+	 * @param bForceUpdate If true, forces the visibility update even if the current visibility is the same as the new
+	 * visibility. Must be used for initial visibility update.
+	 */
+	void UpdateComponentVisibility(const FGameplayTime& CurrentGameTime, const bool bForceUpdate = false);
+
+	// Current visibility of the component. Used to avoid unnecessary visibility changes.
+	bool bCurrentVisibility = false;
+};
