996aad9ad23dc583448c79cfec7f12a752312200
Driver2579 <driver2579@gmail.com>
2025-06-16 01:53:17 +0300

    Implemented a fragment for item classification

---
+++ Source/EscapeChronicles/Public/Objects/InventoryItemFragments/ClassificationInventoryItemFragment.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Objects/InventoryItemFragment.h"
+#include "ClassificationInventoryItemFragment.generated.h"
+
+enum class EItemClassification : uint8;
+
+// A fragment that simply adds a classification to the item (EItemClassification)
+UCLASS()
+class ESCAPECHRONICLES_API UClassificationInventoryItemFragment : public UInventoryItemFragment
+{
+	GENERATED_BODY()
+
+public:
+	EItemClassification GetItemClassification() const { return ItemClassification; }
+
+private:
+	UPROPERTY(EditAnywhere)
+	EItemClassification ItemClassification;
+};
