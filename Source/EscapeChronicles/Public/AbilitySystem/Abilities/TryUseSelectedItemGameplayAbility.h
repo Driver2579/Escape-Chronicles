37698baa8d070cd2db51af4924b46983aaa8b00c
Driver2579 <driver2579@gmail.com>
2025-06-15 16:49:01 +0300

    Implemented an ability to use a selected item

---
+++ Source/EscapeChronicles/Public/AbilitySystem/Abilities/TryUseSelectedItemGameplayAbility.h
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "EscapeChroniclesGameplayAbility.h"
+#include "TryUseSelectedItemGameplayAbility.generated.h"
+
+/**
+ * An ability that tries to use the currently selected item in the inventory. CommitAbility will be called only if the
+ * item was used successfully.
+ */
+UCLASS()
+class ESCAPECHRONICLES_API UTryUseSelectedItemGameplayAbility : public UEscapeChroniclesGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UTryUseSelectedItemGameplayAbility();
+
+protected:
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo,
+		const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+};
