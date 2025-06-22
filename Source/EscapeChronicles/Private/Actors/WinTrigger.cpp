03cc8ca229f4376d33d30dde3f5bd59e78391994
TheSheleng <128699288+TheSheleng@users.noreply.github.com>
2025-06-22 18:01:08 +0300

    Added AWinTrigger  and UWinMenuWidget

---
+++ Source/EscapeChronicles/Private/Actors/WinTrigger.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Actors/WinTrigger.h"
+
+#include "GameState/EscapeChroniclesGameState.h"
+
+void AWinTrigger::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (!HasAuthority()) return;
+
+	const APawn* OtherPawn = Cast<APawn>(OtherActor);
+	if (!ensureAlways(IsValid(OtherPawn))) return;
+
+	const APlayerController* Controller = OtherPawn->GetController<APlayerController>();
+	if (!ensureAlways(IsValid(Controller))) return;
+
+	AEscapeChroniclesGameState* GameState = GetWorld()->GetGameState<AEscapeChroniclesGameState>();
+	if (ensureAlways(IsValid(GameState))) GameState->NetMulticast_WinGame();
+}
---
+++ Source/EscapeChronicles/Public/UI/Widgets/UserWidgets/ActivatableWidgets/RoutableWidget/WinMenuWidget.h
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameInstances/EscapeChroniclesGameInstance.h"
+#include "UI/Widgets/UserWidgets/ActivatableWidgets/RoutableWidget.h"
+#include "UI/Widgets/UserWidgets/Buttons/TextButtonBaseWidget.h"
+#include "WinMenuWidget.generated.h"
+
+UCLASS()
+class ESCAPECHRONICLES_API UWinMenuWidget : public URoutableWidget
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void NativeConstruct() override
+	{
+		Super::NativeConstruct();
+
+		EndButton->OnClicked().AddWeakLambda(this, [this]
+		{
+			UEscapeChroniclesGameInstance* GameInstance =
+				GetWorld()->GetGameInstanceChecked<UEscapeChroniclesGameInstance>();
+
+			if (!ensureAlways(IsValid(GameInstance))) return;
+
+			GameInstance->DestroyHostSession(FOnDestroySessionCompleteDelegate(), true);
+		});
+	}
+
+private:
+	UPROPERTY(Transient, meta=(BindWidget))
+	TObjectPtr<UTextButtonBaseWidget> EndButton;
+
+	
+};
