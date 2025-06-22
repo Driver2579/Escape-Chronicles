51b0b2b79709fc147b2463d9a1b1b6507a9d517b
Driver2579 <driver2579@gmail.com>
2025-06-22 18:20:38 +0300

    Fixed wrong exiting to main menu on client

--- Source/EscapeChronicles/Private/UI/Widgets/UserWidgets/ActivatableWidgets/RoutableWidget/PauseMenuWidget.cpp
+++ Source/EscapeChronicles/Private/UI/Widgets/UserWidgets/ActivatableWidgets/RoutableWidget/PauseMenuWidget.cpp
@@ -7,6 +7,7 @@
 #include "EscapeChronicles/Public/UI/Widgets/UserWidgets/ActivatableWidgets/Prompts/ConfirmationPopup.h"
 #include "GameInstances/EscapeChroniclesGameInstance.h"
 #include "HUDs/EscapeChroniclesHUD.h"
+#include "Kismet/GameplayStatics.h"
 
 void UPauseMenuWidget::NativeOnInitialized()
 {
@@ -65,12 +66,24 @@ void UPauseMenuWidget::OnExitButtonClicked()
 	// Close the session and travel to the main menu if the user confirms the exit
 	ConfirmationExitWidget->OnResult.AddWeakLambda(this, [this](bool bConfirmed)
 	{
-		if (bConfirmed)
+		if (!bConfirmed)
 		{
-			UEscapeChroniclesGameInstance* GameInstance = GetWorld()->GetGameInstanceChecked<
-				UEscapeChroniclesGameInstance>();
+			return;
+		}
 
+		const UWorld* World = GetWorld();
+
+		UEscapeChroniclesGameInstance* GameInstance = World->GetGameInstanceChecked<UEscapeChroniclesGameInstance>();
+
+		// If the world is a server, destroy the session and travel to the main menu
+		if (World->GetNetMode() < NM_Client)
+		{
 			GameInstance->DestroyHostSession(FOnDestroySessionCompleteDelegate(), true);
+		}
+		// Otherwise, use the client travel to the main menu
+		else
+		{
+			GameInstance->ClientTravelToMainMenu(World->GetFirstPlayerController());
 		}
 	});
 }
